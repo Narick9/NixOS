9efeeb198dd95848c5becde4650c9608c5c1f248
artur <muslimow9@yandex.ru>
2022-01-14 14:34:08 +0300

    You've added toshiba "BACKUP_1VO" partition to nix config
            On branch master
            Changes to be committed:
    	   modified:   filesystems.nix

--- filesystems.nix
+++ filesystems.nix
@@ -1,4 +1,13 @@
 { config, ... }:
 
 {
+  fileSystems."/run/media/art/BACKUPS_1VO" =
+    #### .../toshiba - it can't be `.../BACKUPS_1VO/` for some reason
+    {
+      device = "/dev/disk/by-label/BACKUPS_1VO";
+      fsType = "exfat";
+      options = [ "defaults" "user" ];
+      #### "user" - even with this option you (as usual user) can't mount
+      ####   something without sudo
+    };
 }

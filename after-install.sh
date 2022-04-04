77ce08947f5c283769a262c82bc1b8c819a8a8c2
artur <muslimow9@yandex.ru>
2022-04-04 17:32:47 +0300

    The after-install.sh is upgraded
     On branch master
     Changes to be committed:
    	modified:   after-install.sh

--- after-install.sh
+++ after-install.sh
@@ -2,6 +2,8 @@
 
 # These are some thing that nix expressions can't do (maybe can, afterall)
 
+sudo nixos-generate-config
+
 sudo chmod 777 -R /etc/nixos/
 sudo chmod 777 -R /etc/ipython/
 sudo chown -R art /etc/nixos/
